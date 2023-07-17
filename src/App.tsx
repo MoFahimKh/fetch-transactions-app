6daedc0af0db22ea20d6873ca6e23d8488c72ece
jaswinder.khartri@fetch.ai <jaswinder.khartri@fetch.ai>
2023-07-17 15:35:22 +0530

    Add mapping function

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,111 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import "./App.css";
+import { data } from "./constants";
 
 function App() {
-  const [data, setData] = useState("");
-  const [error, setError] = useState("");
+  const [fetchedData, setFetchedData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  interface Amount {
+    amount: string;
+    denom: string;
+  }
+
+  interface Transaction {
+    sender: string;
+    receiver: string;
+    hash: string;
+    type: string;
+    gasLimit: string;
+    amount: Amount[];
+    gas_used: string;
+    height: string;
+  }
+
+  interface Node {
+    address: string;
+    transactions: Transaction[];
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://evmos.numia.xyz/txs/evmos1kdxj7ukuvw5ht93598l0n6t0ye4kvl8x8vj2nr",
-          {
-            headers: {
-              Authorization: "Bearer sk_276dcee2d7ea486cb064a7ae55feb885",
-              "Access-Control-Allow-Origin": "*",
-              mode: "no-cors",
-            },
-          }
-        );
-        setData(JSON.stringify(response.data));
-      } catch (error) {
-        setError("An error occurred while fetching data.");
-        console.log("Error:", error);
-      }
-    };
-
-    fetchData();
+    // Simulating data fetch
+    setFetchedData(data);
+    console.log(data);
   }, []);
+
+  const mapTransactions = (data: any): Node[] => {
+    const { addressIndex, tx, gas_used, height } = data;
+    const { body } = tx;
+    const { messages } = body;
+  
+    const nodes: Node[] = [];
+  
+    for (const address of addressIndex) {
+      const transactions: Transaction[] = [];
+  
+      for (const message of messages) {
+        const { validatorAddress, "@type": type, delegatorAddress } = message;
+  
+        const transaction: Transaction = {
+          sender: validatorAddress,
+          receiver: delegatorAddress,
+          hash: tx.hash,
+          type,
+          gasLimit: tx.authInfo.fee.gasLimit,
+          amount: tx.authInfo.fee.amount,
+          gas_used,
+          height,
+        };
+  
+        transactions.push(transaction);
+      }
+  
+      const node: Node = {
+        address,
+        transactions,
+      };
+  
+      nodes.push(node);
+    }
+  
+    return nodes;
+  };
+  
+
+  const nodes = fetchedData ? mapTransactions(fetchedData) : [];
+  console.log(nodes);
 
   return (
     <div className="App">
       <header className="App-header">
-        {error ? <p>{error}</p> : <p>{data || "Loading..."}</p>}
+        <div>
+          {nodes.map((node, index) => (
+            <div key={index}>
+              <h3>Address: {node.address}</h3>
+              <ul>
+                {node.transactions.map((transaction, index) => (
+                  <li key={index}>
+                    <p>Sender: {transaction.sender}</p>
+                    <p>Receiver: {transaction.receiver}</p>
+                    <p>Hash: {transaction.hash}</p>
+                    <p>Type: {transaction.type}</p>
+                    <p>Gas Limit: {transaction.gasLimit}</p>
+                    <p>Amount:</p>
+                    <ul>
+                      {transaction.amount.map((amt, index) => (
+                        <li key={index}>
+                          Amount: {amt.amount}, Denom: {amt.denom}
+                        </li>
+                      ))}
+                    </ul>
+                    <p>Gas Used: {transaction.gas_used}</p>
+                    <p>Height: {transaction.height}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
---
+++ src/constants.ts
@@ -0,0 +1,67 @@
+export const data = {
+  code: 0,
+  schemaVersion: 3,
+  data: "EmQKPy9jb3Ntb3MuZGlzdHJpYnV0aW9uLnYxYmV0YTEuTXNnV2l0aGRyYXdEZWxlZ2F0b3JSZXdhcmRSZXNwb25zZRIhCh8KBmFldm1vcxIVMTE0NDkzMjAyMzc5MDY5NjAyMjA4",
+  tx: {
+    body: {
+      timeoutHeight: "0",
+      messages: [
+        {
+          validatorAddress:
+            "evmosvaloper1sp9frqwep52chwavv3xd776myy8gyyvkv5uysl",
+          "@type": "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
+          delegatorAddress: "evmos1kdxj7ukuvw5ht93598l0n6t0ye4kvl8x8vj2nr",
+        },
+      ],
+      memo: "",
+      extensionOptions: [],
+      nonCriticalExtensionOptions: [],
+    },
+    signatures: [
+      "d7Ac16i/E1VhgGBFy5xPTFuoVmby+cht32C5cP9rkrJRKNcmTB09liPouasi6wWmbbCz6SQZnGZcVAiIs0KkBQ==",
+    ],
+    authInfo: {
+      signerInfos: [
+        {
+          sequence: "245",
+          publicKey: {
+            typeUrl: "/ethermint.crypto.v1.ethsecp256k1.PubKey",
+            value: "CiECVWOAczLCaSw6UcuGJEkceFLmfBbgW0PqgQ55VOzIYZg=",
+          },
+          modeInfo: {
+            single: {
+              mode: 127,
+            },
+          },
+        },
+      ],
+      fee: {
+        gasLimit: "200000",
+        amount: [
+          {
+            amount: "5000000000000000",
+            denom: "aevmos",
+          },
+        ],
+        payer: "",
+        granter: "",
+      },
+    },
+  },
+  gas_used: "162771",
+  index: 4,
+  codespace: "",
+  chainId: "evmos_9001-2",
+  gas_wanted: "200000",
+  addressIndex: [
+    "evmos1kdxj7ukuvw5ht93598l0n6t0ye4kvl8x8vj2nr",
+    "0xb34d2f72DC63a975963429FEf9e96f266b667ce6",
+    "evmosvaloper1sp9frqwep52chwavv3xd776myy8gyyvkv5uysl",
+    "evmos1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8974jnh",
+    "0x93354845030274cD4bf1686Abd60AB28EC52e1a7",
+  ],
+  hash: "DF74537ECCFEDB53788F24D1591C5459E4252C9BA4AA064155B4199BED397F13",
+
+  height: "14574443",
+  info: "",
+};
